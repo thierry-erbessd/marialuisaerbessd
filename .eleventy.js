9294406030d464a0a280911bd32b20d778daedff
Thierry Erbessd <thierry.erbessd@gmail.com>
2025-12-17 16:50:15 -0600

    Revert "Merge remote-tracking branch 'origin/thierry-codex/configure-eleventy-for-personal-site'"
    This reverts commit 095294de9ae0e3fc1a3ebed8525a682554b29f03, reversing
    changes made to fd1e07d778fe94f76ed5881321ded15df978a0d7.

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,9 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.setTemplateFormats(["njk", "md", "html"]);
-  eleventyConfig.addFilter("year", () => new Date().getFullYear());
-
   return {
-    htmlTemplateEngine: "njk",
-    markdownTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
     dir: {
       input: "src",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
+      output: "_site",
+      includes: "_includes"
     }
   };
 };
